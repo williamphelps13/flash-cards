ff9199aaa8da52d1142311bb52b3ac2ade377554
William Phelps <williamphelps13@gmail.com>
2021-07-08 20:54:17 -0600

    Add game class to connect all other classes

--- src/Game.js
+++ src/Game.js
@@ -1,17 +1,34 @@
+const Card = require('./Card');
 const data = require('./data');
 const prototypeQuestions = data.prototypeData;
 const util = require('./util');
+const Deck = require('./Deck');
+const Round = require('./Round');
+
 
 class Game {
-  constructor() {}
-
-  printMessage(deck, round) {
-      console.log(`Welcome to FlashCards! You are playing with ${deck.countCards()} cards.
+  constructor() {
+  }
+  
+  start() {
+    const cards = prototypeQuestions.map(card => new Card(
+      card.id,
+      card.question,
+      card.answers,
+      card.correctAnswer
+    ));
+    const deck = new Deck(cards);
+    this.currentRound = new Round(deck);
+    this.printMessage(deck, this.currentRound);
+    this.printQuestion(this.currentRound);
+  }
+  printMessage(deck) {
+    console.log(`Welcome to FlashCards! You are playing with ${deck.countCards()} cards.
 -----------------------------------------------------------------------`)
   }
 
   printQuestion(round) {
-      util.main(round);
+    util.main(round);
   }
 }
 
